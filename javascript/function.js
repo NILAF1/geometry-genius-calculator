fa5fba53a5460e66b2f1a4db9cb40904ef92fd87
Tasmit101 <btn0.nilaf@gmail.com>
2023-08-19 19:49:52 +0600

    add output bar

---
+++ javascript/function.js
@@ -0,0 +1,19 @@
+// document.querySelector('#triangle-btn').addEventListener('click', function(){
+//    const x = getValue('triangle-input-01');
+
+//    const y = getValue('triangle-input-02');
+
+//    const output = (0.5 * x * y);
+  
+//    const setValue = document.getElementById('li-container');
+//    const li = document.createElement('li');
+//    li.innerText= output;
+//    setValue.appendChild(li);
+// })
+
+function toClick(){
+const fstInput = document.getElementById('rectangle-input-01');
+const lstInput = document.getElementById('rectangle-input-02');
+
+
+}
---
+++ javascript/utility.js
@@ -0,0 +1,12 @@
+function getValue(x){
+const firstValueX = document.getElementById(x);
+
+const firstValueString = firstValueX.value;
+
+const valueX = parseFloat(firstValueString);
+
+
+return valueX;
+
+}
+
